519570773011e9d575fa82d1517c540fb79f9c56
John de Robles <jrbderobles@gmail.com>
2022-10-13 06:22:39 +0800

    Added Pages and their Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Container, Navbar } from 'react-bootstrap';
+import { Link, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import NotFoundPage from './Pages/NotFoundPage';
+// import Tictactoe from './Game/TicTacToe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <Navbar expand='lg' variant='light' bg='light'>
+          <Container>
+            <Link className='nav-link' to='/'>Home</Link>
+            <Link className='nav-link' to='/about'>About Me</Link>
+            <Link className='nav-link' to='/tictactoe'>Tic Tac Toe</Link>
+          </Container>
+        </Navbar><br />
+
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          {/* <Route path='/tictactoe' element={<Tictactoe />} /> */}
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
